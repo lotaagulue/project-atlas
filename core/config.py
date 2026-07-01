e9fea373794f772e86a67344e973ae18e18c41e8
lotaagulue <lotaagulue@gmail.com>
2026-06-30 22:39:29 -0400

    add dotenv integration to load environment variables from .env file

--- core/config.py
+++ core/config.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()  # populate os.environ from .env so connectors' os.getenv(...) calls see it too
 
 
 class Settings(BaseSettings):
